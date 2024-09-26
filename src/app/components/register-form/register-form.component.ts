7c529cb5f15308d699e8173b946c3f4ceb489a3d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-26 14:54:59 -0500

    [+] register-form.component*: Estructura formulario & Funcionalidad [+] main-page.component*: Implementacion de RegisterFormComponent

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+  registerForm: FormGroup;
 
+  constructor( private fb: FormBuilder ) {
+    this.registerForm = this.fb.group({
+      name: [ '', [ Validators.required ] ],
+      email: [ '', [ Validators.required, Validators.email ] ],
+    });
+  }
+
+  onSubmit() {
+    if ( this.registerForm.valid ) {
+      const formData = this.registerForm.value;
+      // Aquí enviarías los datos al componente de la lista
+      console.log( formData );
+    }
+  }
 }
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RegisterFormComponent } from '../../components/register-form/register-form.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RegisterFormComponent
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
