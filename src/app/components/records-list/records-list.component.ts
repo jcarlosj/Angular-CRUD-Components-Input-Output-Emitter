94ed7ba78c707c671dafce8ea08c8e7416acba66
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-26 14:55:45 -0500

    [+] ng generate component components/records-list --skip-tests

---
+++ src/app/components/records-list/records-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-records-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './records-list.component.html',
+  styleUrl: './records-list.component.css'
+})
+export class RecordsListComponent {
+
+}
