0cd46aabf91e560ea5c4b69af6efd231b4959d3c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-26 15:20:22 -0500

    [+] main-page.component*: Implementa RegisterFormComponent & RecordsListComponent

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RegisterFormComponent } from '../../components/register-form/register-form.component';
+import { RecordsListComponent } from '../../components/records-list/records-list.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
-    RegisterFormComponent
+    RegisterFormComponent,
+    RecordsListComponent
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+  recordsList: Array<{ name: string; email: string }> = [];
 
+  addNewRecord(record: { name: string; email: string }) {
+    console.log(record);
+    this.recordsList.push( record );
+  }
 }
